03eab5d51c89a4cc88cf870bd5a9a1fdb3fbdb04
Yacin Tmimi <yacintmimi@gmail.com>
2022-07-22 07:29:58 -0400

    Add env variable to control the AWS Region
    Some users might not have the region set by default, so we'll let them
    set it via an env variable or default to `us-east-1`.

--- offensiveAdsFlagger/views.py
+++ offensiveAdsFlagger/views.py
@@ -40,12 +40,14 @@ transcribe_client = boto3.client(
     "transcribe",
     aws_access_key_id=settings.AWS_ACCESS_KEY,
     aws_secret_access_key=settings.AWS_SECRET_KEY,
+    region_name=settings.AWS_REGION,
 )
 
 s3_client = boto3.client(
     "s3",
     aws_access_key_id=settings.AWS_ACCESS_KEY,
     aws_secret_access_key=settings.AWS_SECRET_KEY,
+    region_name=settings.AWS_REGION,
 )
 
 
